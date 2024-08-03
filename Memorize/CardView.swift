18c28f95c2d3cfaac0de7ff6c0057c8e1e43ead2
Jarvis Chi <o0taiwan0o@gmail.com>
2024-08-03 08:54:19 -0400

    Created cardify view modifier and view extension

--- Memorize/CardView.swift
+++ Memorize/CardView.swift
@@ -11,38 +11,29 @@ struct CardView: View {
     typealias Card = MemoryGame<String>.Card
 
     let card: Card
-    
+
     init(_ card: Card) {
         self.card = card
     }
-    
+
     var body: some View {
-        ZStack {
-            let base = RoundedRectangle(cornerRadius: Constants.cornerRadius)
-            Group {
-                base.fill(.white)
-                base.strokeBorder(lineWidth: Constants.lineWidth)
-                Pie(endAngle: .degrees(240))
-                    .opacity(Constants.Pie.opacity)
-                    .overlay(
-                        Text(card.content)
-                            .font(.system(size: Constants.FontSize.largest))
-                            .minimumScaleFactor(Constants.FontSize.scaleFactor)
-                            .multilineTextAlignment(.center)
-                            .aspectRatio(1, contentMode: .fit)
-                            .padding(Constants.Pie.inset)
-                    )
-                    .padding(Constants.inset)
-            }
-            .opacity(card.isFaceUp ? 1:0)
-            base.fill()
-                .opacity(card.isFaceUp ? 0 : 1)
-        }
+        Pie(endAngle: .degrees(240))
+            .opacity(Constants.Pie.opacity)
+            .overlay(
+                Text(card.content)
+                    .font(.system(size: Constants.FontSize.largest))
+                    .minimumScaleFactor(Constants.FontSize.scaleFactor)
+                    .multilineTextAlignment(.center)
+                    .aspectRatio(1, contentMode: .fit)
+                    .padding(Constants.Pie.inset)
+            )
+            .padding(Constants.inset)
+            .cardify(isFaceUp: card.isFaceUp)
+            .opacity(card.isFaceUp || !card.isMatched ? 1 : 0)
     }
-    
+
     private struct Constants {
-        static let cornerRadius: CGFloat = 12
-        static let lineWidth: CGFloat = 2
+
         static let inset: CGFloat = 5
         struct FontSize {
             static let largest: CGFloat = 200
@@ -56,22 +47,33 @@ struct CardView: View {
     }
 }
 
-struct CardView_Previews: PreviewProvider{
+struct CardView_Previews: PreviewProvider {
     typealias Card = CardView.Card
     static var previews: some View {
-        VStack{
-            HStack{
-                CardView(Card(isFaceUp: true, isMatched: true, content: "X", id: "test1"))
-                    .aspectRatio(4/3, contentMode: .fit)
+        VStack {
+            HStack {
+                CardView(
+                    Card(
+                        isFaceUp: true, isMatched: true, content: "X",
+                        id: "test1")
+                )
+                .aspectRatio(4 / 3, contentMode: .fit)
                 CardView(Card(content: "X", id: "test1"))
             }
-            HStack{
-                CardView(Card(isFaceUp: true, isMatched: true, content: "This is a very long sting and i hope it fits", id: "test1"))
-                CardView(Card(isFaceUp: true, isMatched: true, content: "X", id: "test1"))
+            HStack {
+                CardView(
+                    Card(
+                        isFaceUp: true, isMatched: true,
+                        content: "This is a very long sting and i hope it fits",
+                        id: "test1"))
+                CardView(
+                    Card(
+                        isFaceUp: true, isMatched: true, content: "X",
+                        id: "test1"))
             }
         }
-            .padding()
-            .foregroundColor(Color.orange)
-        
+        .padding()
+        .foregroundColor(Color.orange)
+
     }
 }
--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -9,42 +9,38 @@ import SwiftUI
 
 struct EmojiMemoryGameView: View {
     @ObservedObject var viewModel: EmojiMemoryGame
-    private let aspectRatio: CGFloat = 2/3
+    private let aspectRatio: CGFloat = 2 / 3
     private let spacing: CGFloat = 4
-    
+
     var body: some View {
-        VStack{
-                cards
-                    .foregroundColor(viewModel.color)
-                    .animation(.default, value: viewModel.cards)
-            Button("Shuffle"){
+        VStack {
+            cards
+                .foregroundColor(viewModel.color)
+                .animation(.default, value: viewModel.cards)
+            Button("Shuffle") {
                 viewModel.shuffle()
             }
         }
         .padding()
     }
-    
 
-    private var cards: some View{
+    private var cards: some View {
         AspectVGrid(viewModel.cards, aspectRatio: aspectRatio) { card in
             CardView(card)
                 .padding(spacing)
                 .onTapGesture {
                     viewModel.choose(card)
                 }
-       }
+        }
     }
-    
+
 }
 
-
-
-struct EmojiMemoryGameView_Previews: PreviewProvider{
-    static var previews: some View{
+struct EmojiMemoryGameView_Previews: PreviewProvider {
+    static var previews: some View {
         EmojiMemoryGameView(viewModel: EmojiMemoryGame())
     }
 }
-
 
 //#Preview {
 //    ContentView()
